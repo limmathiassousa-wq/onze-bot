481d63b4fba9b58da6f62756561062475080c62e
limmathiassousa-wq <limmathiassousa@gmail.com>
2026-09-20 19:38:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -667,8 +667,7 @@ setInterval(() => {
 
     client.once('clientReady', async () => {
     console.log(`Logado como ${client.user.tag}!`);
-    client.user.setActivity(`Prefixo: ${PREFIXO}`, { type: ActivityType.Streaming, url: 'https://youtu.be/XTDH7gSqwiQ?si=_Y7GWvC5q0ANTsmC' });
-
+    client.user.setActivity(`Prefixo: ${PREFIXO}`, { type: ActivityType.Streaming, url: 'https://www.youtube.com/watch?v=XTDH7gSqwiQ' });
     
 carregarTellonymPendentes();
     await carregarProtecao();
