2ad73f01acb81f79e554151c8c7f606074240728
limmathiassousa-wq <limmathiassousa@gmail.com>
2026-09-19 21:15:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const {
 const { logar, enviarLogModeracao, logarBanimento, logarMembro, logarCargo, logarCallTemp, logarExpulsao, logarMute, logarAntiLink, logarAntiSpam, logarAntiBot, logarMensagemApagada, logarMensagemEditada, logarVoz, logarCastigo, logarCargoServidor, logarCanalServidor, logarPunicaoCargosStaff } = require('./logger');
 
 const { anaResponderComAudio } = require('./ana_voz');
-const CANAIS_VOZ_ANA = ['1548489854038581308', '1548578896054718474'];
+const CANAIS_VOZ_ANA = ['1550979191040909402', '1550764596208472117'];
 
 
 // ============ BOT ============
