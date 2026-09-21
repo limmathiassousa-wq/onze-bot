8201220606d03846ddfbeec3b304afa4dabf6b88
limmathiassousa-wq <limmathiassousa@gmail.com>
2026-09-21 10:58:34 -0300

    Fix URL casing in setActivity method
    Updated the setActivity method to use lowercase URL.

--- index.js
+++ index.js
@@ -727,7 +727,10 @@ setInterval(() => {
 
     client.once('clientReady', async () => {
     console.log(`Logado como ${client.user.tag}!`);
-    client.user.setActivity(`Prefixo: ${PREFIXO}`, { type: ActivityType.Streaming, url: 'https://Twitch.tv/discord' });
+    client.user.setActivity(`Prefixo: ${PREFIXO}`, { 
+      type: ActivityType.Streaming, 
+      url: 'https://twitch.tv/discord' // tudo minúsculo
+    });
 
     
 carregarTellonymPendentes();
